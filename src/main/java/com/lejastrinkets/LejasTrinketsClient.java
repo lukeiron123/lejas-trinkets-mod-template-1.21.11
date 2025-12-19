8143f092374700cbb33e7036ca0bbeb4d3c5733c
Erik Backhans <erik.backhans@gmail.com>
2025-12-19 15:38:39 +0100

    Initial commit

---
+++ src/main/java/com/lejastrinkets/LejasTrinketsClient.java
@@ -0,0 +1,11 @@
+package com.lejastrinkets;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LejasTrinketsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/lejastrinkets/LejasTrinketsMod.java
@@ -0,0 +1,15 @@
+package com.lejastrinkets;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LejasTrinketsMod implements ModInitializer {
+	public static final String MOD_ID = "lejas-trinkets-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
