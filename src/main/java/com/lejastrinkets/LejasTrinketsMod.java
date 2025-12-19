f24388375d96d9b3585e3b4d4cd64155423b7126
Erik Backhans <erik.backhans@gmail.com>
2025-12-19 16:58:53 +0100

    Added Emerald Ring to Minecraft

--- src/main/java/com/lejastrinkets/LejasTrinketsMod.java
+++ src/main/java/com/lejastrinkets/LejasTrinketsMod.java
@@ -1,5 +1,6 @@
 package com.lejastrinkets;
 
+import com.lejastrinkets.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,5 +12,6 @@ public class LejasTrinketsMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/com/lejastrinkets/item/ModItems.java
@@ -0,0 +1,32 @@
+package com.lejastrinkets.item;
+
+import com.lejastrinkets.LejasTrinketsMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item EMERALD_RING = registerItem("emerald_ring", new Item(
+            new Item.Settings().registryKey(
+                    RegistryKey.of(
+                            RegistryKeys.ITEM, Identifier.of(LejasTrinketsMod.MOD_ID,"emerald_ring"))
+            )
+    ));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(LejasTrinketsMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        LejasTrinketsMod.LOGGER.info("Registering Mod Items for" + LejasTrinketsMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(EMERALD_RING);
+        });
+    }
+}
