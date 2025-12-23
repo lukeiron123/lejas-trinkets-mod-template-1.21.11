2b30fd735c505b50dee27addde057bd41cf3dfc6
Alva Backhans <alva.backhans@gmail.com>
2025-12-23 01:42:09 +0100

    imports

--- src/main/java/com/lejastrinkets/item/QuiverItem.java
+++ src/main/java/com/lejastrinkets/item/QuiverItem.java
@@ -1,13 +1,6 @@
 package com.lejastrinkets.item;
 
-import net.minecraft.component.Component;
-import net.minecraft.component.ComponentType;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.world.World;
 
 public class QuiverItem extends Item {
 
